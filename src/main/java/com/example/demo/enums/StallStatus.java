9545c8914b5b971481782b1823e998d138959306
lin80092091 <jenny80092@gmail.com>
2026-07-10 18:32:04 +0800

    [add]新增活動攤位狀態enum

---
+++ src/main/java/com/example/demo/enums/StallStatus.java
@@ -0,0 +1,15 @@
+package com.example.demo.enums;
+
+/**
+ * 
+ * 活動攤位狀態，包含AVAILABLE、DISABLED、ASSIGNED、SELECTED
+ * 
+ * @see com.example.demo.entity.EventStall
+ */
+public enum StallStatus {
+    AVAILABLE, 
+    DISABLED, 
+    ASSIGNED, 
+    SELECTED
+    //TODO:需要再確認欄位設定
+}
