bfaad31490298792ef948fc7ccf184179405862c
lin80092091 <jenny80092@gmail.com>
2026-07-11 23:31:24 +0800

    [update]修改User以符合資料庫設定，並將其中的巢狀類別獨立出來，分別新增javadoc

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -13,104 +13,99 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 import com.example.demo.enums.Role;
+import com.example.demo.enums.UserStatus;
 
 /**
- * User
- * 使用者的Entity
+ * 使用者的Entity。<br>
+ * 包含角色類型、email、密碼(加密後)、帳號登入方式、google帳號唯一識別碼、使用者帳號狀態、是否已有登入中的裝置、使用者Email驗證完成時間、使用者自動登出判斷時間、使用者帳號創建時間、使用者帳號更新時間<br>
+ * 
+ * @see MarketEvent#user
+ * @see EventApplication#user
+ * @see UserProfile#user
  */
 @Entity
 @Data
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(name = " UQ_users_email", columnNames = "email"))
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**角色類型 */
     @Enumerated(EnumType.STRING)
     @Column(name = "role", length = 30, nullable = false)
     private Role role;
 
+    /**email */
     @Column(name = "email", length = 255, nullable = false, unique = true)
     private String email;
 
+    /**密碼(加密後)*/
     @Column(name = "password_hash", length = 255)
     private String passwordHash;
 
-    @Column(name = "phone", length = 30)
-    private String phone;
-
-    /**帳號登入方式 */
+    /** 帳號登入方式 */
     @Enumerated(EnumType.STRING)
     @Column(name = "provider", length = 30, nullable = false)
     private Provider provider;
 
+    /**google帳號唯一識別碼 */
     @Column(name = "google_sub", length = 255)
     private String googleSub;
 
-    /**使用者帳號狀態 */
+    /** 使用者帳號狀態 */
     @Enumerated(EnumType.STRING)
     @Column(name = "status", length = 30, nullable = false)
-    private Status status = Status.UNACTIVE;
+    private UserStatus status = UserStatus.UNACTIVE;
 
-    /**是否已有登入中的裝置 */
+    /** 是否已有登入中的裝置 */
     @Column(name = "isLogin", nullable = false)
     private Boolean isLogin = false;
 
-    /**使用者Email驗證完成時間 */
+    /** 使用者Email驗證完成時間 */
     @Column(name = "email_verified_at")
     private LocalDateTime emailVerifiedAt;
 
-    /**使用者自動登出判斷時間*/
+    /** 使用者自動登出判斷時間 */
     @Column(name = "expired_time", nullable = false)
     private LocalDateTime expiredTime;
 
-    /**使用者帳號創建時間 */
+    /** 使用者帳號創建時間 */
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    /**使用者帳號更新時間 */
+    /** 使用者帳號更新時間 */
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    /**使用者舉辦的活動清單 */
+    /** 使用者舉辦的活動清單 */
     @OneToMany(mappedBy = "user")
     private List<MarketEvent> marketEvents;
 
-    /**使用者的攤位報名清單*/
+    /** 使用者的攤位報名清單 */
     @OneToMany(mappedBy = "user")
     private List<EventApplication> eventApplications;
 
-    /**使用者的主辦方/攤主資料清單 */
+    /** 使用者的主辦方/攤主資料清單 */
     @OneToOne(mappedBy = "user")
     private UserProfile userProfile;
 
-    public void setUserProfile(UserProfile userProfile){
+    public void setUserProfile(UserProfile userProfile) {
         this.userProfile = userProfile;
         if (userProfile != null) {
             userProfile.setUser(this);
         }
     }
 
-    /**帳號登入方式 */
+    /** 帳號登入方式 */
     public enum Provider {
         GOOGLE,
         BOTH,
         LOCAL
-    }
-
-    /**使用者帳號狀態 */
-    public enum Status {
-        /**已刪除 */
-        IS_DELETED,
-        /**已停用 */
-        DISABLED,
-        /**活動中 */
-        ACTIVE,
-        /**未激活 */
-        UNACTIVE
     }
 }
---
+++ src/main/java/com/example/demo/enums/UserStatus.java
@@ -0,0 +1,38 @@
+package com.example.demo.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+     * 使用者帳號狀態，包含已刪除、已停用、活動中、未激活
+     */
+    @Getter
+    @RequiredArgsConstructor
+    public enum UserStatus {
+        /** 已刪除 */
+        IS_DELETED("isDelete", "已刪除"),
+        /** 已停用 */
+        DISABLED("disabled", "已停用"),
+        /** 活動中 */
+        ACTIVE("active", "活動中"),
+        /** 未激活 */
+        UNACTIVE("unactive", "未激活");
+
+        @JsonValue
+        private final String status;
+        private final String description;
+
+        @JsonCreator
+        public static UserStatus fromStatus(String status) {
+            for (UserStatus s : values()) {
+                if (s.status.equals(status)) {
+                    return s;
+                }
+            }
+            return null;
+        }
+
+    }
