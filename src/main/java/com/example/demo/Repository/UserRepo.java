7475e227e2caaba07eeb4eedabbdef0cc585a99b
lin80092091 <jenny80092@gmail.com>
2026-07-11 23:32:42 +0800

    [update]移除未使用方法，並根據User變動修改參數類別

--- src/main/java/com/example/demo/Repository/UserRepo.java
+++ src/main/java/com/example/demo/Repository/UserRepo.java
@@ -1,14 +1,14 @@
 package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.example.demo.entity.User;
 import com.example.demo.enums.Role;
-import com.example.demo.entity.User.Status;
+import com.example.demo.enums.UserStatus;
 
 
 
-public interface UserRepo extends JpaRepository<User, Long>{
-    int countByRole(Role role);
-    int countByRoleAndStatus(Role role, Status status);
+public interface UserRepo extends JpaRepository<User, Long>, JpaSpecificationExecutor<User>{
+    int countByRoleAndStatus(Role role, UserStatus status);
 }
