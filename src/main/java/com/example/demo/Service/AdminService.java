d3ffdfaabe459b8de6ee5be23562ab3c64abb11a
lin80092091 <jenny80092@gmail.com>
2026-07-11 21:05:19 +0800

    [update]調整管理員後台: 活動詳細相關方法

--- src/main/java/com/example/demo/Service/AdminService.java
+++ src/main/java/com/example/demo/Service/AdminService.java
@@ -53,8 +53,11 @@ public class AdminService implements AdminServiceInterface, EventStatusServiceIn
     @PersistenceContext
     EntityManager entityManager;
 
+    /** yyyy-MM-dd HH:mm */
     DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    /** yyyy-MM-dd */
     DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    /** HH:mm */
     DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
 
     // 設定管理員後台: 首頁資料統計部分
@@ -84,22 +87,22 @@ public class AdminService implements AdminServiceInterface, EventStatusServiceIn
     // 設定管理員後台: 活動搜尋
     @Override
     public PageResponse<AdminEventsItemDto> getEventsList(AdminEventSearchDto request, int pageNumber, int pageSize) {
-        //----------只撈頁面需要用到的欄位，避免撈出整張表----------
+        // ----------只撈頁面需要用到的欄位，避免撈出整張表----------
         // 設定要join的表
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Tuple> cq = cb.createTupleQuery();
         Root<MarketEvent> root = cq.from(MarketEvent.class);
         Join<MarketEvent, User> user = root.join("user");
         Join<User, UserProfile> userProfile = user.join("userProfile");
-        
+
         // 設定搜尋條件
-        Specification<MarketEvent> spec = EventSpecification.build(request); 
+        Specification<MarketEvent> spec = EventSpecification.build(request);
         Predicate predicate = spec.toPredicate(root, cq, cb);
         if (predicate != null) {
             cq.where(predicate);
         }
 
-        //組裝select欄位
+        // 組裝select欄位
         cq.multiselect(
                 root.get("id"),
                 root.get("coverImageUrl"),
@@ -114,9 +117,9 @@ public class AdminService implements AdminServiceInterface, EventStatusServiceIn
                 root.get("brandPublicAt"),
                 root.get("maxBooths"),
                 EventSpecification.registeredBoothCountSubquery(root, cq, cb));
-        //設定orderBy: 活動創建時間:由新到舊(desc)
+        // 設定orderBy: 活動創建時間:由新到舊(desc)
         cq.orderBy(cb.desc(root.get("createAt")));
-        //查詢結果(有設定limit)
+        // 查詢結果(有設定limit)
         List<Tuple> rows = entityManager.createQuery(cq)
                 .setFirstResult((pageNumber - 1) * pageSize)
                 .setMaxResults(pageSize)
@@ -125,7 +128,7 @@ public class AdminService implements AdminServiceInterface, EventStatusServiceIn
         // 另外查詢符合條件的總筆數
         long total = eventRepo.count(spec);
 
-        //----------設定回傳資料----------
+        // ----------設定回傳資料----------
         List<AdminEventsItemDto> dtoList = new ArrayList<>();
         for (Tuple row : rows) {
             LocalDateTime startAt = row.get(4, LocalDateTime.class);
@@ -158,75 +161,55 @@ public class AdminService implements AdminServiceInterface, EventStatusServiceIn
     // 設定管理員後台: 活動詳細
     @Override
     public AdminEventDetailDto getEventDetail(@NonNull Long eventId) {
-        //----------撈資料----------
-
-        //TODO:定義projection
+        // ----------撈資料----------
+        // FIXME:前端畫面的三種交通方式和marketEvent呈現一對一關係
+        // FIXME:攤位長和寬也和marketEvent呈現一對一關係、攤位高在前端已經取消欄位
         MarketEvent event = eventRepo.findById(eventId)
                 .orElseThrow(() -> new IllegalArgumentException("找不到指定的活動"));
         UserProfile profile = event.getUser().getUserProfile();
         OrganizerProfile organizerProfile = profile.getOrganizerProfile();
-        //----------塞資料----------
-        AdminEventDetailDto dto = new AdminEventDetailDto();
-        //活動基本資料:活動名稱、活動類型
-        dto.setEventName(event.getTitle());
-        dto.setEventType(changeToEventStatus(event).getDescription());
-
+        // ----------塞資料----------
         String eventTime = String.format(
                 "%s ~ %s %s~%s",
                 event.getStartAt().format(dateFormatter),
                 event.getEndAt().format(dateFormatter),
                 event.getStartAt().format(timeFormatter),
                 event.getEndAt().format(timeFormatter));
-        dto.setEventTime(eventTime);
-
-        dto.setLocationName(event.getLocationName());
-        dto.setAddr(String.format("%s%s%s", event.getCity(), event.getDistrict(), event.getAddress()));
-
-        dto.setDescription(event.getDescription());
-        dto.setRegistrationStartTime(event.getRegistrationStartAt().format(dateTimeFormatter));
-        dto.setRegistrationEndTime(event.getRegistrationEndAt().format(dateTimeFormatter));
-        dto.setFinalListConfirmation(
-                event.getBrandPublicAt() == null ? event.getBrandPublicAt().format(dateTimeFormatter) : "-");
-        dto.setActivityTime(eventTime);
-        dto.setOrganizerName(organizerProfile.getCompanyName());
-        dto.setContactPerson(profile.getContactName());
-        dto.setContactPhone(profile.getContactPhone());
-        dto.setEmail(profile.getContactEmail());
-        dto.setAddress(profile.getAddress());
-        dto.setTaxId(organizerProfile.getTaxId());
-
         String serviceHours = String.format(
                 "%s %s~$s",
                 organizerProfile.getServiceDays(),
                 organizerProfile.getServiceStartTime().format(timeFormatter),
                 organizerProfile.getServiceEndTime().format(timeFormatter));
-        dto.setServiceHours(serviceHours);
 
-        // TODO:詢問交通方式欄位
-        dto.setMrt(event.getTrafficInfo());
-        dto.setBus(null);
-        dto.setDrivingDirections(null);
-
-        String boothSpec = String.format("%d * %d", event.getEventStalls().getFirst().getWidth(),
-                event.getEventStalls().getFirst().getLength());
-        dto.setBoothSpec(boothSpec);
-
-        dto.setBoothCount(event.getMaxBooths().toString());
-        dto.setBoothPrice(event.getBaseFee().toString());
-
-        List<EventStallZone> stallZones = event.getEventStallZones();
-        List<BoothZone> dtoBoothZones = new ArrayList<>();
-        for (EventStallZone stallZone : stallZones) {
-            BoothZone boothZone = new BoothZone();
-            boothZone.setName(stallZone.getZoneName());
-            boothZone.setQty(stallZone.getStallCount());
-            dtoBoothZones.add(boothZone);
-        }
-        dto.setBoothZones(dtoBoothZones);
-
-        dto.setBoothLayoutImage(event.getMapImageUrl());
-        dto.setLogs(null); // TODO:等問清楚系統LOG記錄再做
-
+        AdminEventDetailDto dto = new AdminEventDetailDto(
+                event.getTitle(),
+                event.getCoverImageUrl(),
+                event.getCategory().getName(),
+                eventTime,
+                event.getLocationName(),
+                String.format("%s%s%s", event.getCity(), event.getDistrict(), event.getAddress()),
+                eventId,
+                changeToEventStatus(event).getStatus(),
+                event.getDescription(),
+                event.getRegistrationStartAt().format(dateTimeFormatter),
+                event.getRegistrationEndAt().format(dateTimeFormatter),
+                event.getPublicInfoAt() == null ? "-" : event.getPublicInfoAt().format(dateTimeFormatter),
+                organizerProfile.getCompanyName(),
+                profile.getContactName(),
+                profile.getContactPhone(),
+                profile.getContactEmail(),
+                String.format("%s%s%s", profile.getCity(), profile.getDistrict(), profile.getAddress()),
+                organizerProfile.getTaxId(),
+                serviceHours,
+                null,
+                null,
+                null,
+                null,
+                event.getMaxBooths().toString(),
+                event.getBaseFee().toString(),
+                null,
+                event.getMapImageUrl(),
+                null);
         return dto;
     }
 
