e31a5b3a3f0977f345ed9ebc248fdab4986b6e75
lin80092091 <jenny80092@gmail.com>
2026-07-11 23:35:07 +0800

    [update]將class改為record，並新增javadoc

--- src/main/java/com/example/demo/dto/response/admin/AdminUserItemDto.java
+++ src/main/java/com/example/demo/dto/response/admin/AdminUserItemDto.java
@@ -1,27 +1,28 @@
 package com.example.demo.dto.response.admin;
 
+import java.time.LocalDateTime;
+
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import lombok.Data;
 
-/**管理員使用者管理item */
-@Schema(description = "管理員使用者管理列表")
-@Data
-public class AdminUserItemDto {
-
-    /**使用者id */
-    private Long id;
-    /**使用者名稱 */
-    private String name;
-    /**使用者email */
-    @Email
-    private String email;
-    /**使用者角色(參照 {@link com.example.demo.enums.Role}) */
-    private String role;
-    /**創建時間 yyyy-MM-dd HH:mm */
-    private String createdAt;
-    /**最後登入時間 yyyy-MM-dd HH:mm */
-    private String lastLoginAt;
-    /**使用者帳號狀態(參照 {@link com.example.demo.entity.User.Status}) */
-    private String status;
-}
+/**
+ * 
+ * 管理員:使用者管理列表項目
+ * @param id 使用者id
+ * @param name 使用者名稱
+ * @param email 使用者email
+ * @param role 使用者角色 (參照 {@link com.example.demo.enums.Role})
+ * @param createdAt 創建時間 yyyy-MM-dd HH:mm 
+ * @param lastLoginAt 最後登入時間 yyyy-MM-dd HH:mm
+ * @param status 使用者帳號狀態(參照 {@link com.example.demo.entity.User.Status})
+ * 
+ */
+@Schema(description = "管理員:使用者管理列表項目")
+public record AdminUserItemDto(
+    Long id,
+    String name,
+    String email,
+    String role,
+    LocalDateTime createdAt,
+    LocalDateTime lastLoginAt,
+    String status
+) {} 
