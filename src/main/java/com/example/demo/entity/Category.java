f5a6cd749e79f18a5476f27869c8a2830c34a3e0
lin80092091 <jenny80092@gmail.com>
2026-07-11 21:02:59 +0800

    [add]新增javadoc

--- src/main/java/com/example/demo/entity/Category.java
+++ src/main/java/com/example/demo/entity/Category.java
@@ -13,8 +13,10 @@ import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 /**
- * Category
- * 分類的Entity
+ * 活動分類的Entity。<br>
+ * 包含:分類名稱、分類代碼、分類是否啟用
+ * 
+ * @see MarketEvent
  */
 @Entity
 @Data
@@ -25,12 +27,15 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**分類名稱 */
     @Column(name = "name", length = 100, nullable = false)
     private String name;
 
+    /**分類代碼 */
     @Column(name = "slug", length = 100, nullable = false)
     private String slug;
 
+    /**分類是否啟用*/
     @Column(name = "is_active", nullable = false)
     private Boolean isActive = true;
 
